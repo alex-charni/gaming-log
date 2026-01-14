a477fbd3e287855ad7e87c44e9ad84ba8210719f
Alex Charni <alexcharni@outlook.com>
2026-01-14 20:37:07 +0100

    chore: add environment configurations

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  apiUrl: 'https://wrpriypmoaorobeeqmcc.supabase.co/rest/v1',
+  publicImagesUrl: 'https://wrpriypmoaorobeeqmcc.supabase.co/storage/v1/object/public/images',
+  supabaseAnonKey: 'sb_publishable_5W0EQpV0_7nd05cF75ZE1g_WVmjW3Vj',
+  supabaseUrl: 'https://wrpriypmoaorobeeqmcc.supabase.co',
+  startingYear: 2010,
+  useDataMocks: false,
+  dataMockDelay: 0,
+  useImageMocks: false,
+  production: true,
+};
