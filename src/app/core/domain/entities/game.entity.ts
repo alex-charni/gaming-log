855c864c05f1469fda8100e0a870d2bd283e91f1
Alex Charni <alexcharni@outlook.com>
2026-01-15 14:30:29 +0100

    feat: add game entity

---
+++ src/app/core/domain/entities/game.entity.ts
@@ -0,0 +1,9 @@
+export class GameEntity {
+  constructor(
+    public readonly id: string,
+    public readonly title: string,
+    public readonly platform: string,
+    public readonly rating: 1 | 2 | 3 | 4 | 5,
+    public readonly date?: string,
+  ) {}
+}
