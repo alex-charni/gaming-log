ca7327e0f3e6811c93ba2bc80f8206010c2d8772
Alex Charni <alexcharni@outlook.com>
2026-01-27 14:50:27 +0100

    chore: add configurations to allow proper testing with i18n

---
+++ src/testing/i18-testing.ts
@@ -0,0 +1,19 @@
+import { TranslateLoader, provideTranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
+
+import enTranslations from '@i18n/en.json';
+
+export class JsonTranslationLoader implements TranslateLoader {
+  getTranslation() {
+    return of(enTranslations);
+  }
+}
+
+export const provideI18nTesting = () =>
+  provideTranslateService({
+    loader: {
+      provide: TranslateLoader,
+      useClass: JsonTranslationLoader,
+    },
+    defaultLanguage: 'en',
+  });
