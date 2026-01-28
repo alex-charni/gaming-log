872bc6321c1923fe57bdb513f1b5c6564b38d518
Alex Charni <alexcharni@outlook.com>
2026-01-28 09:20:09 +0100

    feat(home-page): use new home page store in home page components

--- src/app/presentation/pages/home-page/home-page.ts
+++ src/app/presentation/pages/home-page/home-page.ts
@@ -1,109 +1,47 @@
-import { Component, computed, effect, inject, signal } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
-import { map } from 'rxjs';
 
-import { GetFeaturedGamesUseCase, GetGamesByYearUseCase } from '@core/application/use-cases';
-import { environment } from '@environments/environment';
 import { GameCardsGrid, Header, HeroSlider, HorizontalSeparator } from '@presentation/components';
-import { toGameCardModel } from '@presentation/mappers';
-import { toHeroSlideModel } from '@presentation/mappers/game-to-hero-slide.model';
-import { IHeroSlide, IYearCard } from '@presentation/schemas/interfaces';
-import { CardTypes } from '@presentation/schemas/types';
 import { Spinner } from '@presentation/services';
+import { HomePageStore } from '@presentation/stores';
 
 @Component({
   selector: 'app-home-page',
   imports: [GameCardsGrid, Header, HeroSlider, HorizontalSeparator, TranslatePipe],
+  providers: [HomePageStore],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
 export class HomePage {
-  private readonly getGamesByYearUseCase = inject(GetGamesByYearUseCase);
-  private readonly getFeaturedGamesUseCase = inject(GetFeaturedGamesUseCase);
+  protected readonly store = inject(HomePageStore);
   private readonly spinnerService = inject(Spinner);
-
-  protected readonly heroSliderIsLoading = signal(false);
-  protected readonly cardGridIsLoading = signal(false);
-  protected readonly cardGridHasLoadedFirstPage = signal(false);
-
-  private readonly isSpinnerVisible = computed(
-    () =>
-      (!this.cardGridHasLoadedFirstPage() && this.cardGridIsLoading()) ||
-      this.heroSliderIsLoading(),
-  );
-
-  private readonly currentYear = signal(new Date().getFullYear());
-  protected readonly nextYearToLoad = signal(this.currentYear());
-  protected readonly haventReachedLastYear = computed(
-    () => this.nextYearToLoad() >= environment.startingYear,
-  );
-
-  protected cardsCollection = signal<CardTypes[]>([]);
-  protected slidesCollection = signal<IHeroSlide[]>([]);
 
   constructor() {
     this.initEffects();
   }
 
   private initEffects(): void {
-    effect(() => this.spinnerService.setVisible(this.isSpinnerVisible()));
+    effect(() => this.spinnerService.setVisible(this.store.spinner()));
   }
 
   ngOnInit(): void {
     this.fetchFeaturedGames();
-    this.fetchGamesByYear(this.nextYearToLoad());
+    this.fetchGamesByYear(this.store.nextYearToLoad());
   }
 
   protected handleFetchMore(): void {
-    this.fetchGamesByYear(this.nextYearToLoad());
+    this.fetchGamesByYear(this.store.nextYearToLoad());
   }
 
   private fetchFeaturedGames(): void {
-    if (this.heroSliderIsLoading()) return;
+    if (this.store.slidesAreLoading()) return;
 
-    this.heroSliderIsLoading.set(true);
-
-    this.getFeaturedGamesUseCase
-      .execute(3)
-      .pipe(
-        map((games) => {
-          return games.map(toHeroSlideModel);
-        }),
-      )
-      .subscribe((response) => {
-        if (response.length) this.slidesCollection.set([...response]);
-        this.heroSliderIsLoading.set(false);
-      });
+    this.store.getHeroBannerSlidesRx(3);
   }
 
   private fetchGamesByYear(year: number): void {
-    if (this.cardGridIsLoading()) return;
+    if (this.store.cardsAreLoading()) return;
 
-    this.cardGridIsLoading.set(true);
-
-    this.getGamesByYearUseCase
-      .execute(year)
-      .pipe(map((games) => games.map(toGameCardModel)))
-      .subscribe((response) => {
-        const scrollTop = window.scrollY; // hack to avoid scroll problems
-
-        if (response.length) {
-          const yearCard: IYearCard = { id: `${year}-card`, type: 'year', year: `${year}` };
-
-          this.cardsCollection.set([...this.cardsCollection(), yearCard, ...response]);
-          this.nextYearToLoad.set(year - 1);
-
-          this.cardGridIsLoading.set(false);
-          this.cardGridHasLoadedFirstPage.set(true);
-
-          requestAnimationFrame(() => {
-            window.scrollTo({ top: scrollTop }); // hack to avoid scroll problems
-          });
-        } else if (this.nextYearToLoad() >= environment.startingYear) {
-          this.nextYearToLoad.set(year - 1);
-          this.cardGridIsLoading.set(false);
-          this.fetchGamesByYear(this.nextYearToLoad());
-        }
-      });
+    this.store.getCardsRx(year);
   }
 }
--- src/app/presentation/schemas/interfaces/index.ts
+++ src/app/presentation/schemas/interfaces/index.ts
@@ -2,4 +2,3 @@ export * from './game-card.interface';
 export * from './hero-slide.interface';
 export * from './text-card.interface';
 export * from './year-card.interface';
-
