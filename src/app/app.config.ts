065311a755f0aa466b8cc24998457dcdffa72115
Alex Charni <alexcharni@outlook.com>
2026-01-29 17:59:43 +0100

    chore(i18n): change i18n initialization strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,36 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { APP_PROVIDERS } from '@infrastructure/config';
 import { SupabaseInterceptor } from '@infrastructure/http/interceptors';
+import { LanguageService } from '@presentation/services';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideTranslateService({
-      fallbackLang: 'en',
-      lang: 'en',
-    }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([SupabaseInterceptor])),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/assets/i18n/',
+        suffix: '.json',
+      }),
+      fallbackLang: 'en',
+      lang: 'en',
+    }),
+    provideAppInitializer(() => {
+      const languageService = inject(LanguageService);
+      return languageService.initLanguages();
+    }),
     ...APP_PROVIDERS,
   ],
 };
