4ee238e54f1a8c88045be56e335fd053f26cec32
Alex Charni <alexcharni@outlook.com>
2026-01-29 10:48:28 +0100

    feat(directives): add new pulse on click directive

---
+++ src/app/presentation/directives/pulse-on-click.spec.ts
@@ -0,0 +1,8 @@
+import { PulseOnClickDirective } from './pulse-on-click';
+
+describe('PulseOnClickDirective', () => {
+  it('should create an instance', () => {
+    const directive = new PulseOnClickDirective();
+    expect(directive).toBeTruthy();
+  });
+});
