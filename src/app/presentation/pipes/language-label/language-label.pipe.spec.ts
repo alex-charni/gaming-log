433a3291beef2542cca8ded9a4bef32836de3417
Alex Charni <alexcharni@outlook.com>
2026-02-03 15:37:25 +0100

    feat(pipes): add language and theme label pipes

---
+++ src/app/presentation/pipes/language-label/language-label.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { LanguageLabelPipe } from './language-label.pipe';
+
+describe('LanguageLabelPipe', () => {
+  it('create an instance', () => {
+    const pipe = new LanguageLabelPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
