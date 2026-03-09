e7ddc164a62855b2b60d4ed34aa42fd2a88ad203
Alex Charni <alex.charni.dev@outlook.com>
2026-03-09 18:02:17 +0100

    feat(burger-menu): update menu to allow navigations

--- src/app/presentation/components/menu/burger-menu/burger-menu.component.ts
+++ src/app/presentation/components/menu/burger-menu/burger-menu.component.ts
@@ -1,15 +1,24 @@
 import { Component, ElementRef, HostListener, inject, signal, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 import { UiStore } from '@presentation/stores/ui';
+import { HorizontalSeparator } from '@presentation/ui';
 import { BurgerButton } from '../burger-button/burger-button';
 import { LanguageSelectorMenuItem } from '../language-selector-menu-item/language-selector-menu-item';
 import { ThemeSelectorMenuItem } from '../theme-selector-menu-item/theme-selector-menu-item';
 
 @Component({
   selector: 'app-burger-menu',
-  imports: [BurgerButton, LanguageSelectorMenuItem, ThemeSelectorMenuItem],
   templateUrl: './burger-menu.component.html',
   styleUrls: ['./burger-menu.component.scss'],
+  imports: [
+    BurgerButton,
+    LanguageSelectorMenuItem,
+    ThemeSelectorMenuItem,
+    TranslatePipe,
+    HorizontalSeparator,
+  ],
 })
 export class BurgerMenuComponent {
   @ViewChild('menu')
@@ -18,13 +27,25 @@ export class BurgerMenuComponent {
   @ViewChild('anchor', { static: true })
   private anchorRef!: ElementRef<HTMLDivElement>;
 
+  private readonly router = inject(Router);
   protected readonly uiStore = inject(UiStore);
 
   readonly isOpen = signal(false);
 
+  protected readonly menuItems = [
+    { label: 'menu.home', icon: 'fa-house', route: '/home' },
+    // { label: 'menu.filters', icon: 'fa-filter', route: '/filter' },
+    { label: 'common.about', icon: 'fa-circle-question', route: '/about' },
+  ];
+
   protected toggleMenu(value: boolean): void {
     this.isOpen.set(value);
     this.uiStore.setFullScreenBackdrop(value);
+  }
+
+  protected navigate(route: string): void {
+    this.toggleMenu(false);
+    this.router.navigate([route]);
   }
 
   @HostListener('document:click', ['$event'])
