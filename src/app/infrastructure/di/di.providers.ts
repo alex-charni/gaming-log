d38605e7100d3852a1b17b1a4beec1df5fcabc5b
Alex Charni <alex.charni.dev@outlook.com>
2026-03-16 11:24:10 +0100

    feat(infrastructure): update di providers

--- src/app/infrastructure/di/di.providers.ts
+++ src/app/infrastructure/di/di.providers.ts
@@ -1,13 +1,42 @@
-import { GetFeaturedGamesUseCase, GetGamesByYearUseCase } from '@core/application/use-cases';
-import { GamesRepository } from '@core/domain/repositories';
+import {
+  GetFeaturedGamesUseCase,
+  GetGamesByYearUseCase,
+  GetUserUseCase,
+  LoginUseCase,
+  LogoutUseCase,
+} from '@core/application/use-cases';
+import { AuthRepository, GamesRepository } from '@core/domain/repositories';
 import { APP_PARAMS } from '@infrastructure/config/app.params';
 import { GamesRepositoryAdapter } from '@infrastructure/http/api';
+import { AuthRepositoryAdapter } from '@infrastructure/http/api/auth/auth.repository.adapter';
 import { APP_SETTINGS } from '../config/app.tokens';
 
 const APP_SETTINGS_PROVIDERS = {
   provide: APP_SETTINGS,
   useValue: APP_PARAMS,
 };
+
+const AUTH_PROVIDERS = [
+  {
+    provide: AuthRepository,
+    useClass: AuthRepositoryAdapter,
+  },
+  {
+    provide: GetUserUseCase,
+    useFactory: (repository: AuthRepository) => new GetUserUseCase(repository),
+    deps: [AuthRepository],
+  },
+  {
+    provide: LoginUseCase,
+    useFactory: (repository: AuthRepository) => new LoginUseCase(repository),
+    deps: [AuthRepository],
+  },
+  {
+    provide: LogoutUseCase,
+    useFactory: (repository: AuthRepository) => new LogoutUseCase(repository),
+    deps: [AuthRepository],
+  },
+];
 
 const GAMES_PROVIDERS = [
   {
@@ -26,4 +55,4 @@ const GAMES_PROVIDERS = [
   },
 ];
 
-export const APP_PROVIDERS = [APP_SETTINGS_PROVIDERS, ...GAMES_PROVIDERS];
+export const APP_PROVIDERS = [AUTH_PROVIDERS, APP_SETTINGS_PROVIDERS, ...GAMES_PROVIDERS];
