ee2f8578ea3280eec67a8a3509c20bd28055320f
Alex Charni <alex.charni.dev@outlook.com>
2026-03-20 14:40:54 +0100

    chore: code clean-up

--- src/app/core/domain/entities/games/game.entity.spec.ts
+++ src/app/core/domain/entities/games/game.entity.spec.ts
@@ -2,7 +2,7 @@ import { GameEntity } from '@core/domain/entities';
 
 describe('GameEntity', () => {
   it('should return a valid GameEntity', () => {
-    const game = new GameEntity('123', 'Sonic', 'MD', 5, '2025-12-31');
+    const game = new GameEntity('123', 'Sonic', 'MD', 5, '2025-12-31', 'finished');
     expect(game).toBeInstanceOf(GameEntity);
   });
 });
