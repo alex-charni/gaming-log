fb3d979498502d8b339519b4ceb3fc5029357bc5
Alex Charni <alex.charni.dev@outlook.com>
2026-03-25 19:13:46 +0100

    feat(add-featured-game): add new use case

--- src/app/infrastructure/http/api/games/games.repository.adapter.ts
+++ src/app/infrastructure/http/api/games/games.repository.adapter.ts
@@ -12,6 +12,40 @@ import { GameApiResponse } from '@infrastructure/http/schemas';
 export class GamesRepositoryAdapter implements GamesRepository {
   private readonly http = inject(HttpClient);
 
+  public addFeaturedGame(game: GameEntity): Promise<void | null> {
+    const url = `${environment.apiUrl}/featured_games`;
+    const mappedGame = toGameApi(game);
+
+    return firstValueFrom(
+      this.http
+        .post<void>(
+          url,
+          { ...mappedGame, date: mappedGame.date ? mappedGame.date : null },
+          { observe: 'response' },
+        )
+        .pipe(map((response) => response.body)),
+    );
+  }
+
+  public async addFeaturedGameImage(
+    gameId: string,
+    image: File,
+    placeholder: File,
+  ): Promise<[void, void]> {
+    const extension = image.type.split('/')[1] || 'webp';
+
+    const coverPath = `featured/${gameId}.${extension}`;
+    const coverUrl = `${environment.supabaseUrl}/${environment.supabaseStorageEndpoint}/object/images/${coverPath}`;
+
+    const placeholderPath = `featured-placeholders/${gameId}.placeholder.${extension}`;
+    const placeholderUrl = `${environment.supabaseUrl}/${environment.supabaseStorageEndpoint}/object/images/${placeholderPath}`;
+
+    return await Promise.all([
+      firstValueFrom(this.http.post<void>(coverUrl, image)),
+      firstValueFrom(this.http.post<void>(placeholderUrl, placeholder)),
+    ]);
+  }
+
   public addGame(game: GameEntity): Promise<void | null> {
     const url = `${environment.apiUrl}/items`;
     const mappedGame = toGameApi(game);
@@ -19,7 +53,7 @@ export class GamesRepositoryAdapter implements GamesRepository {
     return firstValueFrom(
       this.http
         .post<void>(url, mappedGame, { observe: 'response' })
-        .pipe(map((res) => res.body)),
+        .pipe(map((response) => response.body)),
     );
   }
 
@@ -42,7 +76,7 @@ export class GamesRepositoryAdapter implements GamesRepository {
     const url = `${environment.apiUrl}/featured_games`;
 
     let params = new HttpParams();
-    params = params.append('order', 'date.desc');
+    params = params.append('order', 'created_at.desc');
     if (quantity) params = params.append('limit', quantity);
 
     return this.http.get<GameApiResponse[]>(url, { params, observe: 'response' }).pipe(
