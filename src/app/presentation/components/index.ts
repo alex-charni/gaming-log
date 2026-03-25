fa1274101e9dc24fcf030e8eb5d3370a7dca224e
Alex Charni <alex.charni.dev@outlook.com>
2026-03-25 09:12:49 +0100

    chore: code clean-up

--- src/app/presentation/components/index.ts
+++ src/app/presentation/components/index.ts
@@ -2,5 +2,3 @@ export * from './cards-grid/game-cards-grid/game-cards-grid';
 export * from './forms';
 export * from './header/header';
 export * from './hero-slider/hero-slider';
-export * from './ui';
-
