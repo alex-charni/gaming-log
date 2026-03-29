9db99cd7fb4f6f4e367a6c0e669edd76afb2f48a
Alex Charni <alex.charni.dev@outlook.com>
2026-03-29 18:14:00 +0200

    feat(about-page): use common layouts on about page

--- src/app/presentation/pages/about/about.page.ts
+++ src/app/presentation/pages/about/about.page.ts
@@ -1,16 +1,16 @@
 import { Component, input, signal } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 
-import { Header } from '@presentation/components';
 import { Rating } from '@presentation/components/rating/rating';
 import { TitleBanner } from '@presentation/components/title-banner/title-banner';
-import { HorizontalSeparator } from '@presentation/ui';
+import { PageLayout } from '@presentation/pages/page-layout/page-layout';
+import { ContentCardLayout, HorizontalSeparator } from '@presentation/ui';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrl: './about.page.scss',
-  imports: [Header, TranslatePipe, TitleBanner, HorizontalSeparator, Rating],
+  imports: [TranslatePipe, TitleBanner, HorizontalSeparator, Rating, PageLayout, ContentCardLayout],
 })
 export class AboutPage {
   readonly height = input(400);
