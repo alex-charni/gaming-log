e2bdb7d1b885924c9baf5ec4b5d9e8a89808cf0a
Alex Charni <alex.charni.dev@outlook.com>
2026-03-29 18:06:32 +0200

    feat(layout): add common page layout

---
+++ src/app/presentation/pages/page-layout/page-layout.ts
@@ -0,0 +1,20 @@
+import { Component, computed, input } from '@angular/core';
+
+import { Header } from '@presentation/components';
+
+@Component({
+  selector: 'app-page-layout',
+  templateUrl: './page-layout.html',
+  styleUrl: './page-layout.scss',
+  imports: [Header],
+})
+export class PageLayout {
+  readonly centerVertically = input(false);
+  readonly contentMaxSize = input<string>('xl');
+  readonly showHeader = input(true);
+
+  protected readonly classes = computed(() => ({
+    [`page__container--${this.contentMaxSize()}`]: true,
+    'page__container--center-Y': this.centerVertically(),
+  }));
+}
