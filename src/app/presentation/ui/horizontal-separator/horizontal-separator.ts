6daa61061533d0ffebb71ee6002963e3f8aec5ec
Alex Charni <alex.charni.dev@outlook.com>
2026-03-31 09:26:47 +0200

    feat(horizontal-separator): update component to allow heading levels

--- src/app/presentation/ui/horizontal-separator/horizontal-separator.ts
+++ src/app/presentation/ui/horizontal-separator/horizontal-separator.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-horizontal-separator',
@@ -8,4 +8,13 @@ import { Component, input } from '@angular/core';
 export class HorizontalSeparator {
   readonly color = input<'fuchsia' | 'white'>('fuchsia');
   readonly text = input('');
+  readonly titleLevel = input<'h1' | 'h2' | 'h3'>('h2');
+
+  protected readonly separatorClasses = computed(() => ({
+    [`separator__line--${this.color()}`]: true,
+  }));
+
+  protected readonly textClasses = computed(() => ({
+    [`separator__text--${this.color()}`]: true,
+  }));
 }
