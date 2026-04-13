2dd8feab1a0f5f6e973ca0de7a290f9757a86da7
Alex Charni <alex.charni.dev@outlook.com>
2026-04-13 12:28:30 +0200

    feat(button): update button for better customization

--- src/app/presentation/ui/button/button.ts
+++ src/app/presentation/ui/button/button.ts
@@ -1,6 +1,7 @@
-import { Component, input, output } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 
 import { PulseOnClickDirective } from '@presentation/directives';
+import { Sizes } from '@presentation/schemas/types';
 
 @Component({
   selector: 'app-button',
@@ -10,13 +11,20 @@ import { PulseOnClickDirective } from '@presentation/directives';
 })
 export class Button {
   readonly disabled = input<boolean>(false);
-  readonly text = input.required<string>();
   readonly type = input.required<'button' | 'submit'>();
 
+  readonly fontSize = input<Sizes>('sm');
   readonly height = input<string>('3rem');
   readonly width = input<string>('100%');
-
+  readonly round = input(false);
+  readonly style = input<'primary' | 'danger'>('primary');
   readonly action = output();
+
+  protected readonly classes = computed(() => ({
+    [`button--${this.style()}`]: true,
+    [`button--font-size-${this.fontSize()}`]: true,
+    [`button--round`]: this.round(),
+  }));
 
   protected handleClick(): void {
     if (this.type() !== 'submit') this.action.emit();
